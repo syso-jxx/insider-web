2728c90b567b4019d1296bf3916e078d0344cdc5
KimByeongHwa <iambyeonghwa@gmail.com>
2025-01-06 00:37:49 +0900

    chore: react-router-dom 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-export default function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import MyPage from './pages/MyPage';
+
+const Router = () => {
   return (
-    <h1 className='text-primary text-3xl font-bold underline'>Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='signin' element={<SignIn />} />
+        <Route path='mypage' element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
+};
+
+export default function App() {
+  return <Router />;
 }
